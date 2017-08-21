fda554c3fbba7493a763056b6eeaaccc73bbf3b4
steven <stevenle2011@gmail.com>
2017-08-21 12:01:22 -0700

    add other projects

--- views/home.jsx
+++ views/home.jsx
@@ -3,13 +3,13 @@ import React,{Component} from 'react';
 function Home(){
 	return(
 		<div className='topDiv'>
-		<h1>Steven Le</h1>
-		<h3>Full Stack Web Development</h3>
-		<h4>Front-End:</h4>
-		<p>React, Redux, D3, Passport</p>
-		<p>Jquery, Bootstrap</p>
-		<h4>Back-End:</h4>
-		<p>Node, Express, MongoDB, Mongoose</p>
+			<h1>Steven Le</h1>
+			<h3>Full Stack Web Development</h3>
+			<h4>Front-End:</h4>
+			<p>React, Redux, D3, Passport</p>
+			<p>Jquery, Bootstrap</p>
+			<h4>Back-End:</h4>
+			<p>Node, Express, MongoDB, Mongoose</p>
 		</div>
 	)
 
--- views/projects.jsx
+++ views/projects.jsx
@@ -3,38 +3,71 @@ import React,{Component} from 'react';
 function Projects(){
 	return(
 		<div className='topDiv'>
-			<h2>Some projects I've worked on:</h2>
-			<h3>MERN:</h3>
-			<p></p>
+			<h1>MERN:</h1>
+			<p>MERN Projects are hosted on Heroku, so it may 
+			take a while to load if the site is inactive.</p>
 			<div className='flexBox'>
 				<span className='flex-item' >
 					<a href='https://stormy-badlands-58815.herokuapp.com/'>
-						<img src='http://i.imgur.com/Zu0DH0q.png'/>
+						<img src='http://i.imgur.com/y1QwjDv.png'/>
 					</a>
-					<p className='title'>Pinterest Clone</p>
+					<h3>Pinterest Clone</h3>
+					<p>Tech: MERN + Twitter OAuth</p>
+					<p>Info: Post image to website</p>
+					<p>and visit other users' pages.</p>
 				</span>
 				<span className='flex-item' >
 					<a href='https://murmuring-bastion-36829.herokuapp.com/'>
-						<img src='http://i.imgur.com/0veHEe3.png'/>
+						<img src='http://i.imgur.com/Lp429eX.png'/>
 					</a>
+					<h3>Stock Market Chart</h3>
+					<p>Tech: MERN + Websocket + D3</p>
+					<p>Info: Chart opening stock market prices</p>
+					<p>with live update using websocket.</p>
 				</span>
 				<span className='flex-item' >
 					<a href='https://fast-brushlands-52388.herokuapp.com/'>
-						<img src='http://i.imgur.com/4QV79in.png'/>
+						<img src='http://i.imgur.com/uRdSoKO.png'/>
 					</a>
+					<h3>Book Trading</h3>
+					<p>Tech: MERN + Passport</p>
+					<p>Info: Personal booklist with</p>
+					<p>ability to trade with others.</p>
 				</span>
 			</div>
-			<h3>React:</h3>
+			<h1>React:</h1>
 			<div className='flexBox'>
-				<a className='flex-item'><img src=''/></a>
-				<a className='flex-item'><img src=''/></a>
-				<a className='flex-item'><img src=''/></a>
+				<span className='flex-item'>
+					<a href='https://lesteven.github.io/roguelike/'>
+						<img src='http://i.imgur.com/h0NYaQQ.png'/>
+					</a>
+					<h3>Roguelike</h3>
+					<p>Tech: React + Rot.js</p>
+					<p>Roguelike game using Rot.js library.</p>
+				</span>
+				<span className='flex-item'>
+					<a href='https://lesteven.github.io/gameoflifev2/'>
+						<img src='http://i.imgur.com/N6BJum3.png'/>
+					</a>
+					<h3>Game of Life</h3>
+					<p>Tech: React</p>
+					<p>Conway's Game Of Life on HTML canvas.</p>
+				</span>
 			</div>
-			<h3>D3:</h3>
+			<h1>D3:</h1>
 			<div className='flexBox'>
-				<a className='flex-item'><img src=''/></a>
-				<a className='flex-item'><img src=''/></a>
-				<a className='flex-item'><img src=''/></a>
+				<span className='flex-item'>
+					<a><img src='http://i.imgur.com/IKNJ8Jk.png'/></a>
+					<h3>Heatmap</h3>
+					<p>Tech: D3</p>
+					<p>Heat map of land temperature.</p>
+				</span>
+				<span className='flex-item'>
+					<a><img src='http://i.imgur.com/CQvabhw.png'/></a>
+					<h3>Globe</h3>
+					<p>Tech: D3</p>
+					<p>Map of meteorite landings.</p>
+				</span>
 			</div>
 		</div>
 	)
