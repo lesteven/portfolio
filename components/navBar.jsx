0fba843e1cceef879075ecd7ab1f2fb2bec8b214
steven <stevenle2011@gmail.com>
2017-08-20 20:38:14 -0700

    update router again

--- components/navBar.jsx
+++ components/navBar.jsx
@@ -35,7 +35,7 @@ function NavBar(){
 					<Link to ='/contact' className='nav-right'>Contact</Link>
 				</nav>
 
-				<Route exact path= '/' component ={Home}/>
+				<Route exact path= '/portfolio' component ={Home}/>
 				<Route exact path= '/about' component ={About}/>
 				<Route exact path= '/projects' component ={Projects}/>
 				<Route exact path= '/contact' component ={Contact}/>
