710687ea065babd5598e164cc1ee221213fe3c59
steven <stevenle2011@gmail.com>
2017-08-21 12:58:24 -0700

    fix images

--- views/projects.jsx
+++ views/projects.jsx
@@ -9,7 +9,7 @@ function Projects(){
 			<div className='flexBox'>
 				<span className='flex-item' >
 					<a href='https://stormy-badlands-58815.herokuapp.com/'>
-						<img src='http://i.imgur.com/y1QwjDv.png'/>
+						<img src='https://i.imgur.com/y1QwjDv.png'/>
 					</a>
 					<h3>Pinterest Clone</h3>
 					<p>Tech: MERN + Twitter OAuth</p>
@@ -18,7 +18,7 @@ function Projects(){
 				</span>
 				<span className='flex-item' >
 					<a href='https://murmuring-bastion-36829.herokuapp.com/'>
-						<img src='http://i.imgur.com/Lp429eX.png'/>
+						<img src='https://i.imgur.com/Lp429eX.png'/>
 					</a>
 					<h3>Stock Market Chart</h3>
 					<p>Tech: MERN + Websocket + D3</p>
@@ -27,7 +27,7 @@ function Projects(){
 				</span>
 				<span className='flex-item' >
 					<a href='https://fast-brushlands-52388.herokuapp.com/'>
-						<img src='http://i.imgur.com/uRdSoKO.png'/>
+						<img src='https://i.imgur.com/uRdSoKO.png'/>
 					</a>
 					<h3>Book Trading</h3>
 					<p>Tech: MERN + Passport</p>
@@ -39,7 +39,7 @@ function Projects(){
 			<div className='flexBox'>
 				<span className='flex-item'>
 					<a href='https://lesteven.github.io/roguelike/'>
-						<img src='http://i.imgur.com/h0NYaQQ.png'/>
+						<img src='https://i.imgur.com/h0NYaQQ.png'/>
 					</a>
 					<h3>Roguelike</h3>
 					<p>Tech: React + Rot.js</p>
@@ -47,7 +47,7 @@ function Projects(){
 				</span>
 				<span className='flex-item'>
 					<a href='https://lesteven.github.io/gameoflifev2/'>
-						<img src='http://i.imgur.com/N6BJum3.png'/>
+						<img src='https://i.imgur.com/N6BJum3.png'/>
 					</a>
 					<h3>Game of Life</h3>
 					<p>Tech: React</p>
@@ -57,13 +57,13 @@ function Projects(){
 			<h1>D3:</h1>
 			<div className='flexBox'>
 				<span className='flex-item'>
-					<a><img src='http://i.imgur.com/IKNJ8Jk.png'/></a>
+					<a><img src='https://i.imgur.com/IKNJ8Jk.png'/></a>
 					<h3>Heatmap</h3>
 					<p>Tech: D3</p>
 					<p>Heat map of land temperature.</p>
 				</span>
 				<span className='flex-item'>
-					<a><img src='http://i.imgur.com/CQvabhw.png'/></a>
+					<a><img src='https://i.imgur.com/CQvabhw.png'/></a>
 					<h3>Globe</h3>
 					<p>Tech: D3</p>
 					<p>Map of meteorite landings.</p>
