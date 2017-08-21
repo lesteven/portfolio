46529f2e1ed4f262fd5cdccfce2f54b41cf9abcf
steven <stevenle2011@gmail.com>
2017-08-21 12:33:04 -0700

    update about section

--- views/about.jsx
+++ views/about.jsx
@@ -5,13 +5,12 @@ function About(){
 		<div className='topDiv'>
 			<h2>A little bit about me:</h2>
 			<p>I graduated with a BS in Clinical Nutrition (2015)</p>
-			<p>and after a little bit of soul searching,</p>
-			<p> I decided it wasn't for me.</p>
+			<p>and decided not to pursue the field.</p>
 			<br/>
-			<p>On Nov 2016, I stumbled across an online CS course</p>
-			<p> and never looked back!</p>
+			<p>On Nov 2016, I stumbled across an online</p>
+			<p>CS course and never looked back!</p>
 			<br/>
-			<p>My goal is to keep constantly improve </p>
+			<p>My goal is to constantly improve </p>
 			<p>and to explore many other aspects of CS!</p>
 		</div>
 	)
